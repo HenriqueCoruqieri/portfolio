6988dc68e7cb85bf8642de5939c8b811f382c7a0
HenriqueCoruqieri <heoliveirac@gmail.com>
2026-08-22 15:15:31 -0300

    chore: add tailwindcss prettier plugin setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import "./App.css";
+import "./App.css"
 
 function App() {
   return (
-    <div className="w-full h-full bg-black">
+    <div className="h-full w-full bg-black">
       <div className="bg-red-500">
         <p>Olá mundo</p>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
