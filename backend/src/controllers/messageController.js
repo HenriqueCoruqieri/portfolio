a6930f7bdc4943deac8e93a6075bdc1806507781
HenriqueCoruqieri <heoliveirac@gmail.com>
2026-08-23 19:35:56 -0300

    feat: add messageController

---
+++ backend/src/controllers/messageController.js
@@ -0,0 +1,38 @@
+import Message from "../models/Message.js"
+
+export async function getMessage(req, res) {
+  try {
+    const messages = await Message.find().sort({ createdAt: -1 })
+    res.status(200).json(messages)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: "Erro ao buscar mensagem" })
+  }
+}
+
+export async function sendMessage(req, res) {
+  try {
+    const message = await Message.create(req.body)
+    res.status(201).json(message)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: "Erro ao enviar mensagem" })
+  }
+}
+
+export async function deleteMessage(req, res) {
+  try {
+    const message = await Message.findByIdAndDelete(req.params.id)
+
+    if (!message) {
+      return res
+        .status(404)
+        .json({ message: "Não foi possível enviar a mensagem" })
+    }
+
+    res.status(200).json({ message: "Mensagem deletada com sucesso" })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: "Erro ao enviar mensagem" })
+  }
+}
