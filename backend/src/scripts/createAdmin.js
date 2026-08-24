2defba18b2f03749083fd9de216b4c155c3296c3
HenriqueCoruqieri <heoliveirac@gmail.com>
2026-08-24 12:42:47 -0300

    chore: add script to create admin user

---
+++ backend/src/scripts/createAdmin.js
@@ -0,0 +1,36 @@
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+
+import User from "../models/User.js"
+
+dotenv.config()
+
+async function createAdmin() {
+  const email = process.argv[2]
+  const password = process.argv[3]
+
+  if (!email || !password) {
+    console.error("Uso: node src/scripts/createAdmin.js <email> <senha>")
+  }
+
+  try {
+    await mongoose.connect(process.env.DATABASE_URL)
+
+    const userCount = await User.countDocuments()
+
+    if (userCount > 0) {
+      console.error("Já existe um usuário cadastrado")
+      process.exit(1)
+    }
+
+    const user = await User.create({ email, password })
+    console.log(`Usuário "${user.email}" criado com sucesso!`)
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  } finally {
+    await mongoose.disconnect()
+  }
+}
+
+createAdmin()
