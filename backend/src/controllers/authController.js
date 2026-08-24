aaf4a155dde1feb7fadf4ce68a71ce5ffe90c2f6
HenriqueCoruqieri <heoliveirac@gmail.com>
2026-08-24 12:37:28 -0300

    feat: add authController

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,31 @@
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+
+import User from "../models/User.js"
+
+export async function login(req, res) {
+  try {
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email })
+
+    if (!user) {
+      return res.status(401).json({ message: "Credenciais inválidas" })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Credenciais inválidas" })
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    })
+
+    res.status(200).json({ token })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: "Erro ao realizar login" })
+  }
+}
--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcryptjs"
 import mongoose from "mongoose"
 
 const userSchema = new mongoose.Schema(
@@ -7,5 +8,12 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true },
 )
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next()
+
+  this.password = await bcrypt.hash(this.password, 10)
+  next()
+})
 
 export default mongoose.model("User", userSchema)
